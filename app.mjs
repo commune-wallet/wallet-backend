20532b556db37b5010d9e9864530b19139675236
duchess31 <michjoy.se@gmail.com>
2024-03-28 09:38:50 -0400

    feat: auth

---
+++ app.mjs
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import router from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cookieParser());
+
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/api", router);
+
+export default app;
