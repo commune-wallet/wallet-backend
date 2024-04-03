3c227c575de3cfc0a84c4aa0c3052d041de1d46a
lucky-elsa <beautylotus718@gmail.com>
2024-04-03 02:34:57 -0700

    fix: cors error

--- app.mjs
+++ app.mjs
@@ -9,11 +9,22 @@ const app = express();
 
 app.use(cookieParser());
 
+app.use(cookieParser());
 
-app.use(cors());
+const corsOptions = {
+  origin: true, // Replace with your allowed origin(s)
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Specify the HTTP methods allowed
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify the allowed request headers
+  credentials: true, // Enable sending cookies across different domains
+  preflightContinue: false, // Disable preflight requests caching
+  optionsSuccessStatus: 200, // Set the response status code for successful OPTIONS requests
+};
+
+app.use(cors(corsOptions));
+
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api", router);
 
